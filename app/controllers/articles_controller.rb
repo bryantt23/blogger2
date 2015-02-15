4fb3f65c2c62cf82550199a1169842afbd942746
bryant tee <bryantt23@gmail.com>
2015-02-14 19:37:01 -0800

    Deleting Articlesb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+include ArticlesHelper
 
 def index
   @articles = Article.all
@@ -7,5 +8,26 @@ end
 def show
 @article = Article.find(params[:id])
 end
+
+def new
+@article = Article.new
+end
+
+def create
+  # @article = Article.new
+  # @article.title = params[:article][:title]
+  # @article.body = params[:article][:body]
+  
+  # @article = Article.new(params[:article])
+  
+  @article = Article.new(article_params)
+
+  @article.save
+  redirect_to article_path(@article)
+end
+
+
+
+
 
 end
