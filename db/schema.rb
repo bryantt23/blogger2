52d7e39b2a830a7179b6a67e74acd2c765e4bcdf
bryant tee <bryantt23@gmail.com>
2015-02-14 23:36:49 -0800

    comments seem to work

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150215023544) do
+ActiveRecord::Schema.define(version: 20150215040738) do
 
   create_table "articles", force: true do |t|
     t.string   "title",      limit: nil
@@ -19,5 +19,15 @@ ActiveRecord::Schema.define(version: 20150215023544) do
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
   end
+
+  create_table "comments", force: true do |t|
+    t.string   "author_name"
+    t.text     "body"
+    t.integer  "article_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "comments", ["article_id"], name: "index_comments_on_article_id"
 
 end
